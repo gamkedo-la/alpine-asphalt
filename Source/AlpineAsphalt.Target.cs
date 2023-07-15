3b9b8f9c3fa8b03b184ccf00396eb0b31c351374
Patrick Moffett <Patrick@moffett.dev>
2023-07-15 11:56:29 -0700

    Replaced Template Vehicles with Muscle Car

---
+++ Source/AlpineAsphalt.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AlpineAsphaltTarget : TargetRules
+{
+	public AlpineAsphaltTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "AlpineAsphalt" } );
+	}
+}
---
+++ Source/AlpineAsphalt/Public/AAPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AAPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALPINEASPHALT_API AAAPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
