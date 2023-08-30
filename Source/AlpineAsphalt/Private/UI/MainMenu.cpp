70935eac03908f1379f0312076720b6f86d5bbf1
Tyler Funk <tcfunk25@gmail.com>
2023-08-30 15:12:29 -0500

    started work on main menu

---
+++ Source/AlpineAsphalt/Private/UI/MainMenu.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/MainMenu.h"
+
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+
+DEFINE_LOG_CATEGORY(LogMenu)
+
+bool UMainMenu::Initialize()
+{
+	const bool InitSuccess = Super::Initialize();
+
+	if (!InitSuccess)
+	{
+		UE_LOG(LogMenu, Error, TEXT("MainMenu failed to Initialize"))
+		return false;
+	}
+
+	// Make sure all our widgets exist.
+	if (!ensure(WidgetSwitcher != nullptr)) return false;
+	if (!ensure(SettingsWidget != nullptr)) return false;
+	if (!ensure(CreditsWidget != nullptr)) return false;
+
+	// Set up click handlers for top-level buttons.
+	if (!ensure(PlayButton != nullptr)) return false;
+	PlayButton->OnClicked.AddDynamic(this, &UMainMenu::Play);
+
+	if (!ensure(SettingsButton != nullptr)) return false;
+	SettingsButton->OnClicked.AddDynamic(this, &UMainMenu::ShowSettings);
+	
+	if (!ensure(CreditsButton != nullptr)) return false;
+	CreditsButton->OnClicked.AddDynamic(this, &UMainMenu::ShowCredits);
+	
+	if (!ensure(QuitButton != nullptr)) return false;
+	QuitButton->OnClicked.AddDynamic(this, &UMainMenu::Quit);
+
+	return true;
+}
+
+void UMainMenu::Play()
+{
+	auto World = GetWorld();
+	if (!ensure(World != nullptr))
+	{
+		UE_LOG(LogMenu, Error, TEXT("MainMenu failed to get World reference"))
+		return;
+	}
+	
+	World->ServerTravel("/Game/VehicleTemplate/Maps/SmallLevelTest");
+}
+
+void UMainMenu::ShowSettings()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(1);
+	// WidgetSwitcher->SetActiveWidget(SettingsWidget);
+}
+
+void UMainMenu::ShowCredits()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(2);
+	// WidgetSwitcher->SetActiveWidget(CreditsWidget);
+}
+
+void UMainMenu::Quit()
+{
+	auto World = GetWorld();
+	if (!ensure(World != nullptr))
+	{
+		UE_LOG(LogMenu, Error, TEXT("MainMenu failed to get World reference"))
+		return;
+	}
+
+	GetOwningPlayer()->ConsoleCommand("quit");
+}
---
+++ Source/AlpineAsphalt/Public/UI/MainMenu.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+/**
+ * 
+ */
+DECLARE_LOG_CATEGORY_EXTERN(LogMenu, Log, All);
+UCLASS()
+class ALPINEASPHALT_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool Initialize() override;
+
+	void Play();
+	void ShowSettings();
+	void ShowCredits();
+	void Quit();
+
+	/** Widgets **/
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UWidgetSwitcher* WidgetSwitcher;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UWidget* SettingsWidget;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UWidget* CreditsWidget;
+
+	/** Buttons **/
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UButton* PlayButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UButton* SettingsButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UButton* CreditsButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	class UButton* QuitButton;
+
+private:
+	int32 ActiveWidgetIndex;
+};
