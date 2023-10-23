c92bbbdc40fcd0f7b1ccab32128fd48a55af297c
Patrick Moffett <Patrick@Moffett.dev>
2023-10-23 10:34:03 -0700

    Add additional GameUserSettings

--- Source/AlpineAsphalt/Public/UI/AA_GameUserSettings.h
+++ Source/AlpineAsphalt/Public/UI/AA_GameUserSettings.h
@@ -27,8 +27,38 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SetAIDifficulty(EAA_AIDifficulty Difficulty);
 
+	UFUNCTION(BlueprintCallable)
+	void SetAutomaticShifting(bool AutomaticShiftingEnabled);
+
+	UFUNCTION(BlueprintCallable)
+	void SetABSBrakesEnabled(bool bABSBrakesEnabled);
+
+	UFUNCTION(BlueprintCallable)
+	void SetTractionControlEnabled(bool bTractionControlEnabled);
+
+	UFUNCTION(BlueprintCallable)
+	void SetRewindTimeEnabled(bool bRewindTimeEnabled);
+
+	UFUNCTION(BlueprintCallable)
+	void SetSpeedUnitKPH(bool bSpeedUnitKPH);
+
 	UFUNCTION(BlueprintPure)
 	EAA_AIDifficulty GetAIDifficulty() const;
+	
+	UFUNCTION(BlueprintPure)
+	bool GetAutomaticShifting() const;
+	
+	UFUNCTION(BlueprintPure)
+	bool GetABSBrakesEnabled() const;
+	
+	UFUNCTION(BlueprintPure)
+	bool GetTractionControlEnabled() const;
+	
+	UFUNCTION(BlueprintPure)
+	bool GetRewindTimeEnabled() const;
+	
+	UFUNCTION(BlueprintPure)
+	bool GetSpeedUnitKPH() const;
 
 public:
 	UPROPERTY(Category = "Notification", Transient, BlueprintAssignable)
@@ -39,6 +69,22 @@ private:
 	// TODO: Defaulting to Hard as AI isn't finished
 	UPROPERTY(Config)
 	EAA_AIDifficulty AIDifficulty{ EAA_AIDifficulty::Hard };
+
+	UPROPERTY(Config)
+	bool IsAutomaticShifting = true;
+
+	UPROPERTY(Config)
+	bool ABSBrakesEnabled = true;
+	
+	UPROPERTY(Config)
+	bool TractionControlEnabled = true;
+
+	UPROPERTY(Config)
+	bool RewindTimeEnabled = true;
+
+	UPROPERTY(Config)
+	bool SpeedUnitKPH = true;
+	
 };
 
 #pragma region Inline Definitions
@@ -53,9 +99,52 @@ inline void UAA_GameUserSettings::SetAIDifficulty(EAA_AIDifficulty Difficulty)
 	AIDifficulty = Difficulty;
 }
 
+inline void UAA_GameUserSettings::SetAutomaticShifting(bool bIsAutomaticShifting)
+{
+	IsAutomaticShifting = bIsAutomaticShifting;
+}
+
+inline void UAA_GameUserSettings::SetABSBrakesEnabled(bool bABSBrakesEnabled)
+{
+	ABSBrakesEnabled = bABSBrakesEnabled;
+}
+
+inline void UAA_GameUserSettings::SetTractionControlEnabled(bool bTractionControlEnabled)
+{
+	TractionControlEnabled = bTractionControlEnabled;
+}
+
+inline void UAA_GameUserSettings::SetRewindTimeEnabled(bool bRewindTimeEnabled)
+{
+	RewindTimeEnabled = bRewindTimeEnabled;
+}
+inline void UAA_GameUserSettings::SetSpeedUnitKPH(bool bSpeedUnitKPH)
+{
+	SpeedUnitKPH = bSpeedUnitKPH;
+}
 inline EAA_AIDifficulty UAA_GameUserSettings::GetAIDifficulty() const
 {
 	return AIDifficulty;
+}
+inline bool UAA_GameUserSettings::GetAutomaticShifting() const
+{
+	return IsAutomaticShifting;
+}
+inline bool UAA_GameUserSettings::GetABSBrakesEnabled() const
+{
+	return ABSBrakesEnabled;
+}
+inline bool UAA_GameUserSettings::GetTractionControlEnabled() const
+{
+	return TractionControlEnabled;
+}
+inline bool UAA_GameUserSettings::GetRewindTimeEnabled() const
+{
+	return RewindTimeEnabled;
+}
+inline bool UAA_GameUserSettings::GetSpeedUnitKPH() const
+{
+	return SpeedUnitKPH;
 }
 
 #pragma endregion Inline Definitions
